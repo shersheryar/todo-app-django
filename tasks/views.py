2bfd5f2d3fdd6264d084ecd77e8b44adad9e8be4
shersheryar <p218027@pwr.nu.edu.pk>
2025-02-26 12:13:02 +0500

    added form for our project also handled post request for form

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect   
+# from django.http import HttpResponse
+from . models import Task
+from . forms import TaskForm
 
 # Create your views here.
 
 def  index(requrest):
-    return render(requrest, 'tasks.html')
+    form = TaskForm()
+    tasks = Task.objects.all()
+    
+    if requrest.method == 'POST':
+        form = TaskForm(requrest.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'tasks': tasks, "TaskForm": form}
+    return render(requrest, 'tasks.html', context)
+    # return render(requrest, 'tasks.html')
